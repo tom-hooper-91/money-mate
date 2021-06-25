ece0233b5d666f984749a0742d4915fe2bb9da95
Tom Hooper <tomwbhooper@hotmail.com>
2021-06-26 09:55:22 +1200

    add header component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './Header'
 
 export default function App () {
   return (
     <div className='app'>
       <div className='container'>
-        <h1>Hello World</h1>
+        <Header />
       </div>
     </div>
   )
---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header () {
+  return (
+    <div className="row">
+      <div className="col-lg-12">
+        <h1 className='display-1 text-center'>Money Mate</h1>
+      </div>
+    </div>
+  )
+}
