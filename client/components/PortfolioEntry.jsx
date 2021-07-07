101f1190928e520d350994f810cce1e78391f0c8
Tom Hooper <tomwbhooper@hotmail.com>
2021-07-07 17:26:14 +1200

    bad external api calls no longer stop entry from rendering

--- client/components/PortfolioEntry.jsx
+++ client/components/PortfolioEntry.jsx
@@ -12,21 +12,31 @@ function PortfolioEntry ({ entry, quote }) {
   }, [])
   const checkPerformance = (quote, entry) => {
     const equity = quote.find(pos => pos['01. symbol'] === entry.ticker)
-    if (Number(equity['09. change'] > 0)) return 'gainer'
-    else return 'loser'
+    if (equity !== undefined) {
+      if (Number(equity['09. change'] > 0)) return 'gainer'
+      else return 'loser'
+    }
   }
   return (
     <>
       <div className="col-lg-3 dark-background round-edge text-center border m-3 shadow">
-        {entry.name && results.find(pos => pos['01. symbol'])
+        {entry.name
           ? <>
             <hr />
             <h4>{entry.name}</h4>
             <p><em>Ticker: </em> {entry.ticker}</p>
             <p><em>Buy Price: </em> ${entry.buy_price}</p>
-            <p><em>Current Price:</em> ${results.find(pos => pos['01. symbol'] === entry.ticker)['05. price']}</p>
+            <p><em>Current Price:</em> ${results.find(pos => pos['01. symbol'])
+              ? results.find(pos => pos['01. symbol'] === entry.ticker)['05. price']
+              : 'Unavailable'
+            }
+            </p>
             <p><em>Number of Shares: </em>{entry.number_shares}</p>
-            <p><em>Daily Change:</em><span className={checkPerformance(results, entry)}> {results.find(pos => pos['01. symbol'] === entry.ticker)['10. change percent']}</span></p>
+            <p><em>Daily Change:</em><span className={checkPerformance(results, entry)}> {results.find(pos => pos['01. symbol'])
+              ? results.find(pos => pos['01. symbol'] === entry.ticker)['10. change percent']
+              : 'Unavailable'
+            }
+            </span></p>
           </>
           : <h4>Loading....</h4>
         }
