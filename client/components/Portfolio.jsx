977e0aae299e68457db4d5d186029f945f796a9b
Tom Hooper <tomwbhooper@hotmail.com>
2021-07-07 17:17:05 +1200

    add local state to portfolio to check for bad api calls

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -8,7 +8,7 @@ function Portfolio ({ portfolio }) {
       <div className="row justify-content-center">
         <div className="col-lg-4 text-center align-center">
           <h1 className="fw-lighter">Portfolio</h1>
-          <p>Here you can view which stocks you currently have in your portfolio. This information is stored in a database.  You will also see up to date pricing information from the Alhpa Vantage external API</p>
+          <p>Here you can view which stocks you currently have in your portfolio. This information is stored in a database.  You will also see up to date pricing information from the Alpha Vantage external API</p>
         </div>
       </div>
       <div className="row justify-content-center">
--- client/components/PortfolioEntry.jsx
+++ client/components/PortfolioEntry.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 function PortfolioEntry ({ entry, quote }) {
+  const [results, setResults] = useState([])
+  const checkForNull = (quote) => {
+    const arr = quote.filter(pos => pos !== null)
+    return arr.filter(pos => pos !== undefined)
+  }
+  useEffect(() => {
+    setResults(checkForNull(quote))
+  }, [])
   const checkPerformance = (quote, entry) => {
     const equity = quote.find(pos => pos['01. symbol'] === entry.ticker)
-    console.log(equity)
     if (Number(equity['09. change'] > 0)) return 'gainer'
     else return 'loser'
   }
-
   return (
     <>
       <div className="col-lg-3 dark-background round-edge text-center border m-3 shadow">
-        <hr />
-        <h4>{entry.name}</h4>
-        <p><em>Ticker: </em> {entry.ticker}</p>
-        <p><em>Buy Price: </em> ${entry.buy_price.toFixed(2)}</p>
-        <p><em>Current Price:</em> ${quote.find(pos => pos['01. symbol'] === entry.ticker)['05. price']}</p>
-        <p><em>Number of Shares: </em>{entry.number_shares}</p>
-        <p><em>Daily Change:</em><span className={checkPerformance(quote, entry)}> {quote.find(pos => pos['01. symbol'] === entry.ticker)['10. change percent']}</span></p>
+        {entry.name && results.find(pos => pos['01. symbol'])
+          ? <>
+            <hr />
+            <h4>{entry.name}</h4>
+            <p><em>Ticker: </em> {entry.ticker}</p>
+            <p><em>Buy Price: </em> ${entry.buy_price}</p>
+            <p><em>Current Price:</em> ${results.find(pos => pos['01. symbol'] === entry.ticker)['05. price']}</p>
+            <p><em>Number of Shares: </em>{entry.number_shares}</p>
+            <p><em>Daily Change:</em><span className={checkPerformance(results, entry)}> {results.find(pos => pos['01. symbol'] === entry.ticker)['10. change percent']}</span></p>
+          </>
+          : <h4>Loading....</h4>
+        }
       </div>
     </>
   )
