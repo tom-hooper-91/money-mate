8e65e383d0d0e996be0eee55d4627d480e541ec1
Tom Hooper <tomwbhooper@hotmail.com>
2021-07-13 10:39:53 +1200

    heroku set up

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
-const config = require('./knexfile').development
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
