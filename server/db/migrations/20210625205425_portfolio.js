a2b33123dd24b72329ee97376fc3a7ddeb6fd241
Tom Hooper <tomwbhooper@hotmail.com>
2021-07-13 12:32:12 +1200

    change data type of buy_price in migration

--- server/db/migrations/20210625205425_portfolio.js
+++ server/db/migrations/20210625205425_portfolio.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     table.increments('id').primary()
     table.string('name')
     table.string('ticker')
-    table.integer('buy_price')
+    table.decimal('buy_price')
     table.integer('number_shares')
   })
 }
